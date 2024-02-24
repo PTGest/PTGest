d2d2aa18703ed810c12822b3da9ba7f48a1b490c
Daniel Sousa <danielcabritasousa@hotmail.com>
2024-02-24 16:42:42 +0000

    Added project structure

---
+++ code/jvm/src/test/kotlin/pt/isel/leic/PTGest/PtGestApplicationTests.kt
@@ -0,0 +1,13 @@
+package pt.isel.leic.PTGest
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PtGestApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
