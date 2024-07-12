84944610bbf51db0b8776eefafb4eda9b598670f
PedroMacedoo <macedostation@gmail.com>
2024-07-12 18:31:09 +0100

    changes

--- code/js/src/services/utils/envUtils.ts
+++ code/js/src/services/utils/envUtils.ts
@@ -1,13 +1,7 @@
-// function getRandomIcon(): string {
-//     const icons: string[] = ["boy.png", "girl.png", "man.png", "man2.png", "man3.png", "user.png", "user2.png", "who.png", "woman.png", "woman2.png"]
-//
-//     const randomIndex = Math.floor(Math.random() * icons.length)
-//     return icons[randomIndex]
-// }
 
 // Example usage
 
-const apiBaseUri = import.meta.env.VITE_API_URI
+const apiBaseUri = import.meta.env.VITE_API_URI || 'http://localhost:8080'
 const vuePort = import.meta.env.VITE_VUE_PORT
 
 export { apiBaseUri, vuePort }
