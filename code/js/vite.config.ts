b63e70e871512fcec491abc6111e297bf6b13550
PedroMacedoo <macedostation@gmail.com>
2024-07-12 17:43:35 +0100

    changes

--- code/js/vite.config.ts
+++ code/js/vite.config.ts
@@ -7,7 +7,7 @@ import path from 'path'
 export default defineConfig({
     plugins: [vue()],
     server: {
-        port: 5173 ,
+        port: Number.parseInt(process.env.VITE_VUE_PORT!) || 5173,
         proxy: {
           '/api': {
             target: 'http://localhost:8080',
